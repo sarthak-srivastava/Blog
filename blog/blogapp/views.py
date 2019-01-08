40b272d36bc944d75ae81449232ded4838a1bc5e
Sarthak Srivastava <sarth1597@gmail.com>
2019-01-08 16:57:21 +0530

    User Registration added!

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
+def register(request):
+    if request.method=='POST':
+        form  = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('/ ')
+    else:
+        form = UserRegisterForm()
+    return render(request,'blogapp/register.html',{'form':form})
+
+
+
+
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
     return render(request,'blogapp/post_list.html',{'posts': posts})
@@ -28,4 +45,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return redirect(request, 'blog/post_edit.html', {'form': form})
