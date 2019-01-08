4360ee481433306fc043acfd28078f46d2429413
Sarthak Srivastava <sarth1597@gmail.com>
2019-01-08 19:14:46 +0530

    Login added

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from blogapp import views as user_views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blogapp.urls')),
+    path('home/',user_views.post_list, name = 'home'), 
     path('register/',user_views.register, name='register'),
+    path('login/',auth_views.LoginView.as_view(template_name = 'blogapp/login.html'), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name = 'blogapp/logout.html'), name='logout'),
+
 ]
--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm, UserRegisterForm
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 # Create your views here.
@@ -11,8 +12,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('/ ')
+            messages.success(request, f'Account created for {username}! Login to continue.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request,'blogapp/register.html',{'form':form})
@@ -20,19 +21,22 @@ def register(request):
 
 
 
-
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
     return render(request,'blogapp/post_list.html',{'posts': posts})
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request , 'blogapp/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     form = PostForm()
     return render(request, 'blogapp/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -45,4 +49,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return redirect(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blogapp/post_edit.html', {'form': form})
