52747d49912a79d248df63b71ba822d6cf3573fe
Sarthak Srivastava <sarth1597@gmail.com>
2019-01-09 09:31:42 +0530

    New post correction| Screenshots added

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -33,9 +33,17 @@ def post_detail(request, pk):
 
 @login_required
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blogapp/post_edit.html', {'form': form})
-
 @login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
