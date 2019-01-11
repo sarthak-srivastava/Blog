0615d13f422add79d54c663bc1df456a59f551ed
Sarthak Srivastava <sarth1597@gmail.com>
2019-01-11 19:38:16 +0530

    Edit post option added. Creator can only edit their post

--- blog/blogapp/tests.py
+++ blog/blogapp/tests.py
@@ -4,8 +4,11 @@ from django.utils import timezone
 from blogapp.forms import PostForm, UserRegisterForm
 from django.contrib.auth.models import User
 from django.contrib.auth import SESSION_KEY
-
+from django.contrib.auth import authenticate, login
+from django.http.request import HttpRequest
 # models test
+
+
 class LogInTest(TestCase):
     def setUp(self):
         self.credentials = {
@@ -18,3 +21,28 @@ class LogInTest(TestCase):
         # should be logged in now
         self.assertTrue(response.context['user'].is_active)
 
+class PostTest(TestCase):
+    
+
+    def create_post(self, title="only a test", body="yes, this is only a test"):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
+        newrequest = HttpRequest()
+        my_view(newrequest)
+
+
+        return Post.objects.create(title=title, text=body, created_date=timezone.now())
+
+    def test_post_creation(self):
+        w = self.create_post()
+        self.assertTrue(isinstance(w, Post))
+        self.assertEqual(w.__unicode__(), w.title)
+def my_view(request):
+    username = request.POST['test']
+    password = request.POST['secret']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
